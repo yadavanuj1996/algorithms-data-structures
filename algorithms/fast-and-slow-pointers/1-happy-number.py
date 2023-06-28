c225192ee297b717105f6e095bd590522af22920
yadav.anuj <yadav.anuj@fixcraft.in>
2023-06-28 18:54:29 +0530

    Add happy number solution.

---
+++ algorithms/fast-and-slow-pointers/1-happy-number.py
@@ -0,0 +1,46 @@
+
+"""
+Statement
+Write an algorithm to determine if a number n is a happy number.
+
+We use the following process to check if a given number is a happy number:
+
+- Starting with the given number n, replace the number with the sum of the squares of its digits.
+- Repeat the process until:
+    - The number equals 1 , which will depict that the given number n is a happy number.
+    - It enters a cycle, which will depict that the given number n is not a happy number.
+Return TRUE if n is a happy number, and FALSE if not.
+
+Constraints
+- 1 <= n <= 2^31-1
+"""
+
+"""
+Time Complexity: O(log n)
+Space Complexity: O(1)
+"""
+def is_happy_number(n):
+    slow = n 
+    fast = sum_of_squared_digits(n)
+    while not fast == 1:
+        if fast == slow:
+            return False
+        
+        slow = sum_of_squared_digits(slow)
+        fast = sum_of_squared_digits(sum_of_squared_digits(fast))
+        
+    return True
+
+
+# helper function to that will return sum of square of digits of a num ex:- 21 -> (2)^2 + (1)^2 = 4+1 = 5
+def sum_of_squared_digits(n):
+    result = 0
+    while n > 0:
+        result += (n % 10 ) ** 2
+        n = n // 10
+    
+    return result
+
+
+
+
