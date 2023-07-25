3eaa353ab313c71f3cb235292ef7d0ff5d00245b
yadav.anuj <yadav.anuj@fixcraft.in>
2023-07-26 04:17:26 +0530

    Update statement indentation.

--- algorithms/sliding-window/7-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ algorithms/sliding-window/7-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -3,7 +3,8 @@
 Minimum Size Subarray Sum
 
 Statement
-Given an array of positive integers, nums, and a positive integer, target, find the minimum length of a contiguous subarray whose sum is greater than or equal to the target. If no such subarray is found, return 0.
+Given an array of positive integers, nums, and a positive integer, target, find the minimum length of a 
+contiguous subarray whose sum is greater than or equal to the target. If no such subarray is found, return 0.
 
 Constraints:
 - 1 ≤ target ≤ 10^9
