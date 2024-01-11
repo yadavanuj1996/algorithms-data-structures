8184c496e37d6ed1a48bdefd1bcc965e8f04a8ca
yadav.anuj <yadav.anuj@fixcraft.in>
2024-01-11 06:57:30 +0530

    Add tabulation solution.

--- algorithms/striver-sheet/a2z-sheet/16-dynamic-programming/4-dp-on-subsequences/6-01-knapsack/6-01-knapsack.py
+++ algorithms/striver-sheet/a2z-sheet/16-dynamic-programming/4-dp-on-subsequences/6-01-knapsack/6-01-knapsack.py
@@ -41,7 +41,7 @@ of index (0 to n) and w (0 to max_weight), thus the time complexity is O(N*W)
 Space Complexity: O(N*W) + O(W), We are using a recursion stack space(O(N)) and a 2D array ( O(N*W)).
 Dp array has size N*W and recursion stack space is O(W) because we are reducing the val of w
 and calling the function again and again.
-"""
+
 
 from os import *
 from sys import *
@@ -91,6 +91,141 @@ for _ in range(test_cases):
     print(get_max_knapsack_val(n-1, max_weight))
         
     
+"""
+
+
+
+"""
+Solution 2: Tabulation Solution
+Time Complexity: O(N*W), Reason: There are two nested loops
+
+Space Complexity: O(N*W) Reason: We are using an external array of size ‘N*W’. Stack Space is eliminated.
+
+
+"""
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+test_cases = int(input())
+
+
+for _ in range(test_cases):
+    n = int(input())
+    weights = input().strip().split(" ")
+    values = input().strip().split(" ")
+    max_weight = int(input())
+    for i in range(len(weights)):
+        if weights[i]:
+            weights[i] = int(weights[i])
+        if values[i]:
+            values[i] = int(values[i])
+
+
+    dp = [[-1 for _ in range(max_weight+1)] for _ in range(len(weights))]
+    # Step 1 : Setup base case
+    for available_weight in range(max_weight+1):
+        if weights[0] <= available_weight:
+            dp[0][available_weight] = values[0]
+        else:
+            dp[0][available_weight] = 0
+    
+    # Step 2 change and accomodate changing parameters i.e., index and w (available_weight or weight left)
+    for index in range(1, len(weights)):
+        for w in range(max_weight+1):
+            # Step 3 copy the recurence
+            # pick
+            pick = 0
+            if w-weights[index] >= 0:
+                pick = values[index] + dp[index-1][w-weights[index]]
+            #unpick
+            unpick = dp[index-1][w]
+
+            dp[index][w] = max(pick, unpick)
+
+         
+ 
+    print(dp[n-1][max_weight])
+        
+
+"""
+Solution 3 : Space optimized tabulation Solution
+
+Time Complexity: O(N*W), Reason: There are two nested loops
+
+Space Complexity: O(W), Reason: We are using an external array of size ‘W+1’ to store only one row.
+
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+test_cases = int(input())
+
+
+for _ in range(test_cases):
+    n = int(input())
+    weights = input().strip().split(" ")
+    values = input().strip().split(" ")
+    max_weight = int(input())
+    for i in range(len(weights)):
+        if weights[i]:
+            weights[i] = int(weights[i])
+        if values[i]:
+            values[i] = int(values[i])
+
+
+    prev = [-1 for _ in range(max_weight+1)]
+    cur = [-1 for _ in range(max_weight+1)]
+    # Step 1 : Setup base case
+    for available_weight in range(max_weight+1):
+        if weights[0] <= available_weight:
+            prev[available_weight] = values[0]
+        else:
+            prev[available_weight] = 0
+    
+    # Step 2 change and accomodate changing parameters i.e., index and w (available_weight or weight left)
+    for index in range(1, len(weights)):
+        for w in range(max_weight, -1, -1):
+            # Step 3 copy the recurence
+            # pick
+            pick = 0
+            if w-weights[index] >= 0:
+                pick = values[index] + prev[w-weights[index]]
+            #unpick
+            unpick = prev[w]
+
+            cur[w] = max(pick, unpick)
+        # updaing prev to cur
+        prev = cur
+
+         
+ 
+    print(prev[max_weight])
+        
+
+"""
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
