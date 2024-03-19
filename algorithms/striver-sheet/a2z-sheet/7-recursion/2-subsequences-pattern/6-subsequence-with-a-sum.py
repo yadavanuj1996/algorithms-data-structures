c523f44fedfc68f2a4598117ec20e4a65472c22c
yadav.anuj <yadav.anuj@fixcraft.in>
2024-03-19 08:29:08 +0530

    Rearranging code blocks.

--- algorithms/striver-sheet/a2z-sheet/7-recursion/2-subsequences-pattern/6-subsequence-with-a-sum.py
+++ algorithms/striver-sheet/a2z-sheet/7-recursion/2-subsequences-pattern/6-subsequence-with-a-sum.py
@@ -7,6 +7,40 @@ https://www.codingninjas.com/studio/problems/subset-sum_630213?utm_source=strive
 """
 
 """
+Iteration 1
+Time Complexity: O(2^n) 
+Space Complexity: O(n)
+"""
+
+"""
+from typing import *
+
+def isSubsetPresent(n:int, k: int, a: List[int]) -> bool:
+    # Write your code here.
+    return find(0, 0 , a, n, k)
+
+def find(index, sum, nums, n, k):
+    if sum > k: 
+        return False
+        
+    if index == n:
+        if sum == k:
+            return True
+        else:
+            return False
+
+    # pick 
+    if find(index+1, sum+nums[index], nums, n, k):
+        return True
+    # unpick
+    if find(index+1, sum, nums, n, k):
+        return True
+    
+    return False
+
+"""
+
+"""
 Iteration 2 (DP Solution)
 
 Time Complexity: O(n * k) 
@@ -46,38 +80,3 @@ def isSubsetPresent(n:int, k: int, a: List[int]) -> bool:
         return False
 
     return find(0, 0)
-
-
-"""
-Iteration 1
-Time Complexity: O(2^n) 
-Space Complexity: O(n)
-"""
-
-"""
-from typing import *
-
-def isSubsetPresent(n:int, k: int, a: List[int]) -> bool:
-    # Write your code here.
-    return find(0, 0 , a, n, k)
-
-def find(index, sum, nums, n, k):
-    if sum > k: 
-        return False
-        
-    if index == n:
-        if sum == k:
-            return True
-        else:
-            return False
-
-    # pick 
-    if find(index+1, sum+nums[index], nums, n, k):
-        return True
-    # unpick
-    if find(index+1, sum, nums, n, k):
-        return True
-    
-    return False
-
-"""
