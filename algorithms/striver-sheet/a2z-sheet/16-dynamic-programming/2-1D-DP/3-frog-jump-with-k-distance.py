96481fadb2be0a123c08ddea73b23ad23959364a
Anuj Yadav <yadav.anuj.1996@gmail.com>
2024-06-17 08:56:25 +0530

    Update 3-frog-jump-with-k-distance.py

--- algorithms/striver-sheet/a2z-sheet/16-dynamic-programming/2-1D-DP/3-frog-jump-with-k-distance.py
+++ algorithms/striver-sheet/a2z-sheet/16-dynamic-programming/2-1D-DP/3-frog-jump-with-k-distance.py
@@ -40,65 +40,24 @@ Time Complexity: O(N*K),  n is total no of steps and k is no of steps allowed
 Space Complexity: O(N)
 
 """
-# Approach 2: Tabulation
-
-from os import *
-from sys import *
-from collections import *
-from math import *
-
 from typing import *
 
-
-def frogJump(n: int, heights: List[int]) -> int:
-    if n == 1:
-        return 0
-
-    memo = [-1]*n
-
-    memo[0] = 0
-    memo[1] = abs(heights[1] - heights[0])
-    for i in range(2, n):
-        memo[i] = min(
-            abs(heights[i] - heights[i-1]) + memo[i-1], 
-            abs(heights[i] - heights[i-2]) + memo[i-2]
-            )
-
-    return memo[n-1]
-
-
-
-# Approach 1, memoization
-"""
-from os import *
-from sys import *
-from collections import *
-from math import *
-
-from typing import *
-
-  
-def frogJump(n: int, heights: List[int]) -> int:
+def minimizeCost(n : int, k : int, heights : List[int]) -> int:
+    # Write your code here.
     memo = [-1]*n
 
     def min_energy_req(n):
-        if n == 0:
-            return 0
-        
         if n == 1:
-            memo[1] = abs(heights[1] - heights[0])
-            return memo[1]
-        
-        if not memo[n] == -1:
-            return memo[n]
-        
-        memo[n] = min(
-                    abs(heights[n]-heights[n-1]) + min_energy_req(n-1),
-                    abs(heights[n]-heights[n-2]) + min_energy_req(n-2)
-            )
-        return memo[n]
-    
-    return min_energy_req(n-1)
+            return 0
 
+        memo[0] = 0
+        memo[1] = abs(heights[1] - heights[0])
+        for i in range(2, n):
+            for j in range(i, k)
+            memo[i] = min(
+                abs(heights[i] - heights[i-1]) + memo[i-1], 
+                abs(heights[i] - heights[i-2]) + memo[i-2]
+                )
 
-"""
+        return memo[n-1]
+
